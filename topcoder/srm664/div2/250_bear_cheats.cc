fc59866d1732dc507e445600cf4f2d6f92d2aa28
Hrvoje Gazibara <hgazibara@gmail.com>
2015-08-02 20:36:06 +0200

    Replaced tabs with spaces in files from last commit

--- topcoder/srm664/div2/250_bear_cheats.cc
+++ topcoder/srm664/div2/250_bear_cheats.cc
@@ -2,21 +2,21 @@
 
 class BearCheats {
 public:
-	std::string eyesight(int A, int B) {
-		int different = 0;
+    std::string eyesight(int A, int B) {
+        int different = 0;
 
-		while (A != 0) {
-			int a_digit = A % 10;
-			int b_digit = B % 10;
+        while (A != 0) {
+            int a_digit = A % 10;
+            int b_digit = B % 10;
 
-			if (a_digit != b_digit) {
-				++different;
-			}
+            if (a_digit != b_digit) {
+                ++different;
+            }
 
-			A = A / 10;
-			B = B/10;
-		}
+            A = A / 10;
+            B = B/10;
+        }
 
-		return different <= 1 ? "happy" : "glasses";
-	}
+        return different <= 1 ? "happy" : "glasses";
+    }
 };
