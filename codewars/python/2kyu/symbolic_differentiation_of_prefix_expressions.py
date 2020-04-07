ac1a08b5be0797fc9c21e70429324312c5c20732
Hgazibara <hgazibara@gmail.com>
2020-04-07 22:11:32 +0200

    Prepare initial code structure

--- codewars/python/2kyu/symbolic_differentiation_of_prefix_expressions.py
+++ codewars/python/2kyu/symbolic_differentiation_of_prefix_expressions.py
@@ -1,2 +1,53 @@
+import abc
+
+
+class Operand(object):
+    pass
+
+
+class Operator(object):
+    pass
+
+
+class Expression(abc.ABC):
+    
+    @abc.abstractclassmethod
+    def from_string(cls, expression):
+        pass
+    
+    def derive(self):
+        pass
+        
+    def simplify(self):
+        pass
+
+
+class UnaryExpression(Expression):
+    
+    def __init__(self, operator, operand):
+        self.operator = operator
+        self.operand = operand
+
+    def derive(self):
+        pass
+        
+    def simplify(self):
+        pass
+
+
+class BinaryExpression(Expression):
+    
+    def __init__(self, operator, left_operand, right_operand):
+        self.operator = operator
+        self.left_operand = left_operand
+        self.right_operand = right_operand
+    
+    def derive(self):
+        pass
+        
+    def simplify(self):
+        pass
+
+
 def diff(s):
     pass
